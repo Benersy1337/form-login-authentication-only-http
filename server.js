5a69e6b1508ebb3f75401bfe38c81c5120a5e1f1
Benersy1337 <benersy1337@gmail.com>
2023-04-11 13:15:40 -0300

    mudando a porta para o padrao 80

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const users = {
 
 http.createServer(function (req, res) {
     const q = url.parse(req.url, true);
+    console.log(req.method, req.url, req.httpVersion);
     const filename = '.' + q.pathname;
     if (filename == './login') {
         fs.readFile('login.html', function (err, data) {
@@ -22,9 +23,9 @@ http.createServer(function (req, res) {
         });
         req.on('end', function () {
             let userName = body.split('&')[0].split('=')[1];
-            console.log(userName)
+            console.log(`Login: ${userName}`)
             let password = body.split('&')[1].split('=')[1];
-            console.log(password)
+            console.log(`Senha: ${password}`)
             if (users[userName] && users[userName] == password) {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.write('<h1>Login successful!</h1>');
@@ -39,7 +40,7 @@ http.createServer(function (req, res) {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end('404 Not Found');
     }
-}).listen(3000);
+}).listen(80);
 
 
 
